5970c797f67bb5dcc808279f048f542c3bc2f127
Ariv98 <arivanantham@pikcha.com>
2023-01-01 21:00:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import  Navbar  from './components/Navbar';
+import {  Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import Register from './components/Register';
+import Edit from './components/Edit';
+import View from './components/View';
+import {ToastContainer} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/edit/:id" element={<Edit/>} />
+        <Route path="/view/:id" element={<View/>} />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
